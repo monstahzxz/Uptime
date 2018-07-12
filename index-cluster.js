87a4bcb3eeb1f14600409b6100a278deec2a489f
Saran Prasad <saranprasad97@gmail.com>
2018-07-12 15:48:40 +0530

    Create index-cluster.js

---
+++ index-cluster.js
@@ -0,0 +1,37 @@
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+var cli = require('./lib/cli');
+var cluster = require('cluster');
+var os = require('os');
+
+
+
+var app = {};
+
+
+app.init = function(callback){
+
+
+	if(cluster.isMaster){
+		workers.init();
+
+		setTimeout(function(){
+			cli.init();
+			callback();
+		},50);
+
+		for(var i=0;i<os.cpus().length;++i){
+			cluster.fork();
+		}
+	}	
+	else {
+		server.init();
+	}
+
+};
+
+if(require.main === module){
+	app.init(function(){});
+}
+
+module.exports = app;
