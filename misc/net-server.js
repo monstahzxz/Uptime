10887b5266b79ec4aab345f68aafc943ca735f4f
Saran Prasad <saranprasad97@gmail.com>
2018-07-12 15:57:36 +0530

    Create net-server.js

---
+++ misc/net-server.js
@@ -0,0 +1,14 @@
+var net = require('net');
+
+var server = net.createServer(function(connection){
+	var outboundMessage = 'pong';
+	connection.write(outboundMessage);
+
+	connection.on('data',function(inboundMessage){
+		var messageString = inboundMessage.toString();
+		console.log("I wrote " + outboundMessage + " and they said " + messageString);
+	});
+});
+
+
+server.listen(6000);
