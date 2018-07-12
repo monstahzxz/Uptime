973b4f7325fdb1a855eee3cb4262d0ddbcc68637
Saran Prasad <saranprasad97@gmail.com>
2018-07-12 15:57:05 +0530

    Create http2-client.js

---
+++ misc/http2-client.js
@@ -0,0 +1,21 @@
+var http2 = require('http2');
+
+var client = http2.connect('http://localhost:6000');
+
+var req = client.request({
+	':path' : '/',
+});
+
+var str = '';
+
+req.on('data',function(chunk){
+	str += chunk;
+});
+
+
+req.on('end',function(){
+	console.log(str);
+});
+
+
+req.end();
