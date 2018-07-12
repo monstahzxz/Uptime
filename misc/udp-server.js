6f9e085451715c3432d580512cebbeaaa5a5590b
Saran Prasad <saranprasad97@gmail.com>
2018-07-12 15:58:25 +0530

    Create udp-server.js

---
+++ misc/udp-server.js
@@ -0,0 +1,12 @@
+var dgram = require('dgram');
+
+
+var server = dgram.createSocket('udp4');
+
+server.on('message',function(messageBuffer,sender){
+	var messageString = messageBuffer.toString();
+	console.log(messageString);
+});
+
+
+server.bind(6000);
